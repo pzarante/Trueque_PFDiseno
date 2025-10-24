48d50075c230ad4f055cf452ba60918a7b5dcd8f
Bryan <bemontoya@uninorte.edu.co>
2025-10-24 14:57:41 -0500

    analisis basico de sentimiento en comentarios de oferta

--- modelo_NLP/app/modules/process_offer.py
+++ modelo_NLP/app/modules/process_offer.py
@@ -2,11 +2,18 @@ from flask import Blueprint, request, jsonify
 import re
 import string
 from stopwordsiso import stopwords
+import spacy
 
 process_offer = Blueprint('process_offer', __name__)
 
 # Cargar stopwords en español
 STOPWORDS_ES = stopwords("es")
+
+spcy = spacy.load("es_core_news_sm")
+
+def extract_keywords(text):
+    doc = spcy(text)
+    return [token.text for token in doc if token.pos_ in ["NOUN", "PROPN", "ADJ"]]
 
 def clean_text(text):
     # Convertir a minúsculas
@@ -34,10 +41,11 @@ def create_offer():
     
     clean_title = clean_text(title)
     clean_category = clean_text(category)
-    
+    keywords = extract_keywords(title)
     response = {
         "id": data.get('id', None),
         "title": clean_title,
-        "category": clean_category
+        "category": clean_category,
+        "keywords": keywords
     }
     return jsonify(response)
