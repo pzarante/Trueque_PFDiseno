e36af14b2bc31bfdf2829626228b46d8f4d242cf
AlanSan25 <alansg@uninorte.edu.co>
2025-10-29 00:37:19 -0500

    Implementa endpoint GET /search para búsqueda semantica
    Se crea un endpoint para búsqueda por similitud usando embeddings pero sin procesamiento de resultados

--- modelo_NLP/app/modules/semantic_search.py
+++ modelo_NLP/app/modules/semantic_search.py
@@ -6,5 +6,23 @@ semantic_search = Blueprint('semantic_search', __name__)
 
 @semantic_search.route('/search', methods=['GET'])
 def search_offers():
+    query_text = request.args.get("query", "").strip()
+    n = int(request.args.get("n", 5))
+    category = request.args.get("category", "").lower()
 
-    return jsonify({})
+    if not query_text:
+        return jsonify({"error": "Parámetro 'query' es obligatorio"}), 400
+    
+    clear_query = clean_text(query_text)
+
+    query_embedding = text_to_embeddings(clear_query)
+
+    # Para filtros combinados
+    where_filter = {"category": category} if category else {}
+
+    results = current_app.collection.query(
+        query_embeddings=query_embedding,
+        n_results=n
+    )
+    
+    return jsonify({"results":results})
