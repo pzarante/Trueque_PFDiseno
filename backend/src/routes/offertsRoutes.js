06639f5d244029882a96c43680624913eabeabdf
DanielCarracedo <110993227+DanielCarracedo@users.noreply.github.com>
2025-11-20 00:02:32 -0500

    le pettit detalle

--- backend/src/routes/offertsRoutes.js
+++ backend/src/routes/offertsRoutes.js
@@ -1,23 +1,11 @@
 import express from 'express';
-import { createProduct, updateProduct, DeleteProduct, changeStatus } from '../controllers/offertsRobleController.js';
+import { createProduct, updateProduct, DeleteProduct, changeStatus, getAllProducts  } from '../controllers/offertsRobleController.js';
 import upload from '../config/multerConfig.js';
-
-const router = express.Router();
-
-router.post('/OffertCreate', upload.array('imagenes', 3),createProduct);
-router.put('/OffertUpdate',  updateProduct);
-router.delete('/DelateOffert', DeleteProduct);
-router.put('/EditEstado', changeStatus);
-
-export default router;
-
-import { createProduct, updateProduct, DeleteProduct, changeStatus, getAllProducts } from '../controllers/offertsRobleController.js';
-//import recaptchaMiddleware from '../middleware/recaptchaMiddleware.js';
 import { validateFields } from "../middleware/validateFields.js";
 
 const router = express.Router();
 
-router.post('/OffertCreate', /*recaptchaMiddleware,*/ validateFields(['nombre', 'categoria', 'condicionesTrueque', 'comentarioNLP', 'ubicacion']), createProduct);
+router.post('/OffertCreate', /*recaptchaMiddleware,*/upload.array('imagenes', 3), validateFields(['nombre', 'categoria', 'condicionesTrueque', 'comentarioNLP', 'ubicacion']), createProduct);
 router.put('/OffertUpdate', validateFields(['nombre', 'condicionesTrueque', 'comentarioNLP', 'ubicacion']), updateProduct);
 router.delete('/DelateOffert', validateFields(['nombre']), DeleteProduct);
 router.put('/EditEstado', validateFields(['nombre', 'estado']), changeStatus);
